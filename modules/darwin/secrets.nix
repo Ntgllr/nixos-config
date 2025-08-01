2c80d00e7c365d96bf5d0796ac087bf845565d05
Ntgllr <76962922+Ntgllr@users.noreply.github.com>
2025-08-01 16:23:23 +0200

    updated secrets

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -7,41 +7,23 @@ let user = "dgrothe2"; in
       "/Users/${user}/.ssh/id_ed25519"
     ];
 
-    # secrets = {
-    #   "syncthing-cert" = {
-    #     symlink = true;
-    #     path = "/Users/${user}/Library/Application Support/Syncthing/cert.pem";
-    #     file =  "${secrets}/darwin-syncthing-cert.age";
-    #     mode = "644";
-    #     owner = "${user}";
-    #     group = "staff";
-    #   };
+    secrets = {
+      "github-ssh-key" = {
+        symlink = true;
+        path = "/Users/${user}/.ssh/id_github";
+        file =  "${secrets}/github-ssh-key.age";
+        mode = "600";
+        owner = "${user}";
+        group = "staff";
+      };
 
-    #   "syncthing-key" = {
-    #     symlink = true;
-    #     path = "/Users/${user}/Library/Application Support/Syncthing/key.pem";
-    #     file =  "${secrets}/darwin-syncthing-key.age";
-    #     mode = "600";
-    #     owner = "${user}";
-    #     group = "staff";
-    #   };
-
-    #   "github-ssh-key" = {
-    #     symlink = true;
-    #     path = "/Users/${user}/.ssh/id_github";
-    #     file =  "${secrets}/github-ssh-key.age";
-    #     mode = "600";
-    #     owner = "${user}";
-    #     group = "staff";
-    #   };
-
-    #   "github-signing-key" = {
-    #     symlink = false;
-    #     path = "/Users/${user}/.ssh/pgp_github.key";
-    #     file =  "${secrets}/github-signing-key.age";
-    #     mode = "600";
-    #     owner = "${user}";
-    #   };
-    # };
+      "github-signing-key" = {
+        symlink = false;
+        path = "/Users/${user}/.ssh/pgp_github.key";
+        file =  "${secrets}/github-signing-key.age";
+        mode = "600";
+        owner = "${user}";
+      };
+    };
   };
 }
